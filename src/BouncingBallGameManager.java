8955945393b5c38a1c9062d6b3feb24f6dc720bd
Adi Kishony <adi.kishony@mail.huji.ac.il>
2024-01-31 13:52:29 +0200

    added macros

--- src/BouncingBallGameManager.java
+++ src/BouncingBallGameManager.java
@@ -9,16 +9,26 @@ import danogl.util.Vector2;
 import gameobjects.Ball;
 import gameobjects.Paddle;
 
+import java.util.Random;
+
 public class BouncingBallGameManager extends GameManager{
+
+    private static final float BALL_SPEED = 250;
+    private static final int BORDER_WIDTH = 10;
+    private static final int PADDLE_WIDTH = 150;
+    private static final int PADDLE_HEIGHT = 20;
+    private static final int BALL_RADIUS = 35;
+    private static final int DIST_FROM_EDGE_OF_DISPLAY = 30;
+
 
     public BouncingBallGameManager(String windowTitle, Vector2 windowDimensions){
         super(windowTitle, windowDimensions);
     }
 
     private void createWalls(Vector2 windowDimensions){
-        GameObject leftWall = new GameObject(Vector2.ZERO, new Vector2(10,windowDimensions.y()),null);
-        GameObject rightWall = new GameObject(new Vector2(windowDimensions.x(),0), new Vector2(10,windowDimensions.y()),null);
-        GameObject topWall = new GameObject(Vector2.ZERO, new Vector2(windowDimensions.x(),10),null);
+        GameObject leftWall = new GameObject(Vector2.ZERO, new Vector2(BORDER_WIDTH,windowDimensions.y()),null);
+        GameObject rightWall = new GameObject(new Vector2(windowDimensions.x(),0), new Vector2(BORDER_WIDTH,windowDimensions.y()),null);
+        GameObject topWall = new GameObject(Vector2.ZERO, new Vector2(windowDimensions.x(), BORDER_WIDTH),null);
         gameObjects().addGameObject(leftWall,Layer.STATIC_OBJECTS);
         gameObjects().addGameObject(rightWall,Layer.STATIC_OBJECTS);
         gameObjects().addGameObject(topWall,Layer.STATIC_OBJECTS);
@@ -27,9 +37,16 @@ public class BouncingBallGameManager extends GameManager{
     private void createBall(ImageReader imageReader, SoundReader soundReader, Vector2 windowDimensions){
         Renderable ballImage = imageReader.readImage("assets/ball.png", true);
         Sound collisionSound = soundReader.readSound("assets/blop_cut_silenced.wav");
-        GameObject ball = new Ball(Vector2.ZERO, new Vector2(50,50), ballImage, collisionSound);
+        GameObject ball = new Ball(Vector2.ZERO, new Vector2(BALL_RADIUS,BALL_RADIUS), ballImage, collisionSound);
         ball.setCenter(windowDimensions.mult(0.5f));
-        ball.setVelocity(Vector2.DOWN.mult(500));
+        float ballVelY = BALL_SPEED;
+        float ballVelX = BALL_SPEED;
+        Random rand = new Random();
+        if(rand.nextBoolean()){
+            ballVelX *= -1;
+            ballVelY *= -1;
+        }
+        ball.setVelocity(new Vector2(ballVelX,ballVelY));
         gameObjects().addGameObject(ball);
     }
 
@@ -37,17 +54,17 @@ public class BouncingBallGameManager extends GameManager{
         // create user paddle
         GameObject userPaddle = new Paddle(
                 Vector2.ZERO,
-                new Vector2(200,20),
+                new Vector2(PADDLE_WIDTH,PADDLE_HEIGHT),
                 paddleImage,
                 inputListener);
-        userPaddle.setCenter(new Vector2(windowDimensions.x()/2,(int)windowDimensions.y()-30));
+        userPaddle.setCenter(new Vector2(windowDimensions.x()/2,(int)windowDimensions.y()-DIST_FROM_EDGE_OF_DISPLAY));
         gameObjects().addGameObject(userPaddle);
     }
 
     private void createAIPaddle(Renderable paddleImage, Vector2 windowDimensions) {
         //crate ai paddles
-        GameObject aiPaddle = new GameObject(Vector2.ZERO, new Vector2(200, 20), paddleImage);
-        aiPaddle.setCenter(new Vector2(windowDimensions.x() / 2, 30));
+        GameObject aiPaddle = new GameObject(Vector2.ZERO, new Vector2(PADDLE_WIDTH, PADDLE_HEIGHT), paddleImage);
+        aiPaddle.setCenter(new Vector2(windowDimensions.x() / 2, DIST_FROM_EDGE_OF_DISPLAY));
         gameObjects().addGameObject(aiPaddle);
     }
 
