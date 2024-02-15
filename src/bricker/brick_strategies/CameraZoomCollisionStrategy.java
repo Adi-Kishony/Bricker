ef97f0372b7eebfeb2c67be8e8033744c3598bf6
Amit Avnon <amitavnon@cs.huji.ac.il>
2024-02-15 10:55:16 +0200

    wip

--- src/bricker/brick_strategies/CameraZoomCollisionStrategy.java
+++ src/bricker/brick_strategies/CameraZoomCollisionStrategy.java
@@ -8,33 +8,31 @@ import danogl.util.Vector2;
 
 public class CameraZoomCollisionStrategy extends BasicCollisionStrategy implements CollisionStrategy{
     private static final float ZOOM_RATIO = 1.2f;
-    private static Camera camera;
-    private static CameraTracker cameraTracker;
+    private Camera camera;
+    private CameraTracker cameraTracker;
     public CameraZoomCollisionStrategy(BrickerGameManager brickerGameManager) {
         super(brickerGameManager);
-        if(camera == null){
-            camera = new Camera(null, Vector2.ZERO,
-                    brickerGameManager.getWindowDimensions().mult(ZOOM_RATIO),
-                    brickerGameManager.getWindowDimensions());
-        }
-        System.out.println(camera);
-
-        if (cameraTracker == null) {
-            cameraTracker = new CameraTracker(Vector2.ZERO, Vector2.ZERO, null,
-                    brickerGameManager,camera);
-            brickerGameManager.addGameObject(cameraTracker);
-        }
-        System.out.println(cameraTracker);
-
-
+        this.camera = null;
+        this.cameraTracker = null;
 
     }
 
     @Override
     public void onCollision(GameObject obj1, GameObject obj2) {
         super.onCollision(obj1, obj2);
-        if (obj2.getTag().equals("Ball")){
-            cameraTracker.setCameraOn(obj2);
+        if (brickerGameManager.camera() == null){
+            camera = new Camera(null, Vector2.ZERO,
+                    brickerGameManager.getWindowDimensions().mult(ZOOM_RATIO),
+                    brickerGameManager.getWindowDimensions());
+
+            cameraTracker = new CameraTracker(Vector2.ZERO, Vector2.ZERO, null,
+                    brickerGameManager,camera);
+            brickerGameManager.addGameObject(cameraTracker);
+            if (obj2.getTag().equals("Ball")){
+                cameraTracker.setCameraOn(obj2);
+            }
         }
+
+
     }
 }
--- src/bricker/gameobjects/CameraTracker.java
+++ src/bricker/gameobjects/CameraTracker.java
@@ -8,7 +8,7 @@ import danogl.util.Vector2;
 
 public class CameraTracker extends GameObject {
     private final BrickerGameManager brickerGameManager;
-    private final Camera myCamera;
+    private final Camera camera;
     private static final int MAX_BALL_COLLISIONS = 10;
     private final Ball mainBall;
     private int numCollisions;
@@ -16,32 +16,33 @@ public class CameraTracker extends GameObject {
     public CameraTracker(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, BrickerGameManager brickerGameManager, Camera camera) {
         super(topLeftCorner, dimensions, renderable);
         this.brickerGameManager = brickerGameManager;
-        myCamera = camera;
+        this.camera = camera;
         this.mainBall = brickerGameManager.getMainBall();
         this.numCollisions = 0;
     }
 
-    public void setCameraOn(GameObject ball){
-        if(brickerGameManager.camera() == null) {
-            myCamera.setToFollow(ball, Vector2.ZERO);
-            brickerGameManager.setCamera(myCamera);
-            numCollisions = mainBall.getCollisionCounter().value();
-            System.out.println("on");
-        }
+    public void setCameraOn(GameObject ball) {
+        brickerGameManager.setCamera(camera);
+        brickerGameManager.camera().setToFollow(ball, Vector2.ZERO);
+        numCollisions = mainBall.getCollisionCounter().value();
+        System.out.println("on");
     }
 
 
-    private void setCameraOff(){
+    private void setCameraOff() {
         brickerGameManager.setCamera(null);
     }
 
     @Override
     public void update(float deltaTime) {
-        if (mainBall.getCollisionCounter().value() - numCollisions == MAX_BALL_COLLISIONS &&
-                brickerGameManager.camera() != null){
-            System.out.println(mainBall.getCollisionCounter().value() - numCollisions);
-            System.out.println("turnoff");
-            setCameraOff();
+        if (brickerGameManager.camera() != null) {
+            System.out.println("firstIf");
+            if (mainBall.getCollisionCounter().value() - numCollisions >= MAX_BALL_COLLISIONS) {
+                System.out.println(mainBall.getCollisionCounter().value() - numCollisions);
+                System.out.println("turnoff");
+                setCameraOff();
+            }
         }
     }
 }
+
